5ace9d3b3f9289d5b688f841bf57a45cdfd0847e
Anurag Anand <anuraganand789@hotmail.com>
2020-08-25 23:05:24 +0530

    1. Route Detail display feature is refactored to hero detail component. 2. Feature to display messages

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,24 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService : HeroService, private messageService: MessageService) { }
 
+  getHeroes() : void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero) {
       this.selectedHero = hero;
+      this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
